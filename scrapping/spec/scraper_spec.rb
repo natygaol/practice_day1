b207a19e301d2ca0b408af1b4e45d8fb6c6862b6
nataly garcia <naty_garcioli@hotmail.com>
2021-06-25 22:40:31 -0500

    testing 1

---
+++ scrapping/spec/scraper_spec.rb
@@ -0,0 +1,14 @@
+describe "fetch_movies_url" do
+  it "returns an array of movies" do
+    # guardamos el resultado del método en una variable
+    urls = fetch_movies_url
+    expected = [
+      "https://www.imdb.com/title/tt0111161/",
+      "https://www.imdb.com/title/tt0068646/",
+      "https://www.imdb.com/title/tt0071562/",
+      "https://www.imdb.com/title/tt0468569/",
+      "https://www.imdb.com/title/tt0050083/"
+    ]
+    expect(urls).to eq(expected)  
+  end  
+end
