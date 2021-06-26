ec29b12cdcae33b1c2920cd6bf961c6d527250cc
nataly garcia <naty_garcioli@hotmail.com>
2021-06-25 22:35:08 -0500

    seudo coding for web scrapper & added testing file

---
+++ scrapping/web_scrapping.rb
@@ -0,0 +1,4 @@
+# Identificar las 5 primeras peliculas y me guardo el url
+# Accedo a cada película por su url
+# Buscar la información necesaria y guardarla en un hash o array
+# Guardar en movies.yml
